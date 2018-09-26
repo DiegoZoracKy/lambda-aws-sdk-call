15f1627aabfb9bd3775895c057547efc69b9250b
Diego ZoracKy <diegowzk@gmail.com>
2018-09-26 00:09:49 -0300

    Set global configuration object

--- lambda-call-aws-sdk.js
+++ lambda-call-aws-sdk.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 exports.handler = ({AWSConfig, service, serviceOptions, method, params}) => {
     return new Promise((resolve, reject) => {
 
-        new AWS.Config(AWSConfig);
+        AWS.config = new AWS.Config(AWSConfig);
         const serviceInstance = new AWS[service](serviceOptions);
 
         serviceInstance[method](params, (err, res) => {
